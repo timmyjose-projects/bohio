734901d2a060b19bfaa41a91dc3358a45c0c2e61
Timmy Jose <zoltan.jose@gmail.com>
2020-07-16 22:43:19 -0700

    Making Windows and Unix consistent - working Windows API

--- src/os/posix.zig
+++ src/os/posix.zig
@@ -1,15 +1,18 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const process = std.process;
 const os = std.os;
 const fs = std.fs;
 const egg = @import("../lib.zig");
 
-pub fn homeDir() egg.BohioError![]const u8 {
-    return if (os.getenv("HOME")) |home| home else error.HomeNotFound;
+/// Caller needs to free memory
+pub fn homeDir(allocator: *Allocator) egg.BohioError![]const u8 {
+    return if (process.getEnvVarOwned(allocator, "HOME")) |home| home else |_| error.HomeNotFound;
 }
 
+/// Caller needs to free memory
 pub fn clutchHome(allocator: *Allocator) egg.BohioError![]const u8 {
-    if (os.getenv("CLUTCH_HOME")) |clutch_home| {
+    if (process.getEnvVarOwned(allocator, "CLUTCH_HOME")) |clutch_home| {
         if (fs.path.isAbsolute(clutch_home)) {
             return clutch_home;
         }
@@ -22,10 +25,10 @@ pub fn clutchHome(allocator: *Allocator) egg.BohioError![]const u8 {
         } else |_| {
             return egg.BohioError.ClutchHomeNotFound;
         }
+    } else |_| {
+        const home = try homeDir(allocator);
+        return fs.path.joinPosix(allocator, &[_][]const u8{ home, ".clutch" }) catch |_| {
+            return egg.BohioError.ClutchHomeNotFound;
+        };
     }
-
-    const home = try homeDir();
-    return fs.path.joinPosix(allocator, &[_][]const u8{ home, ".clutch" }) catch |_| {
-        return egg.BohioError.ClutchHomeNotFound;
-    };
 }
--- src/os/windows.zig
+++ src/os/windows.zig
@@ -1,33 +1,35 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const process = std.process;
 const os = std.os;
 const fs = std.fs;
 const BohioError = @import("../lib.zig").BohioError;
 const egg = @import("../lib.zig");
 
-pub fn homeDir() egg.BohioError![]const u8 {
-    return if (os.getenvW(@ptrCast([*:0]const u16, "HOME"))) |home| @bitCast([]const u8, home) else error.HomeNotFound;
+/// Caller needs to free memory
+pub fn homeDir(allocator: *Allocator) egg.BohioError![]const u8 {
+    return if (process.getEnvVarOwned(allocator, "USERPROFILE")) |home| @bitCast([]const u8, home) else |_| error.HomeNotFound;
 }
 
+/// Caller needs to free memory
 pub fn clutchHome(allocator: *Allocator) egg.BohioError![]const u8 {
-    if (os.getenvW(@ptrCast([*:0]const u16, "CLUTCH_HOME"))) |clutch_home| {
-        const clutch_home_val = @bitCast([]const u8, clutch_home);
-        if (fs.path.isAbsolute(clutch_home_val)) {
-            return clutch_home_val;
+    if (process.getEnvVarOwned(allocator, "CLUTCH_HOME")) |clutch_home| {
+        if (fs.path.isAbsolute(clutch_home)) {
+            return clutch_home;
         }
 
         var buf: [fs.MAX_PATH_BYTES]u8 = undefined;
         if (os.getcwd(&buf)) |cwd| {
-            return fs.path.joinWindows(allocator, &[_][]const u8{ cwd, clutch_home_val }) catch |_| {
+            return fs.path.joinWindows(allocator, &[_][]const u8{ cwd, clutch_home }) catch |_| {
                 return egg.BohioError.ClutchHomeNotFound;
             };
         } else |_| {
             return egg.BohioError.ClutchHomeNotFound;
         }
+    } else |_| {
+        const home = try homeDir(allocator);
+        return fs.path.joinWindows(allocator, &[_][]const u8{ home, ".clutch" }) catch |_| {
+            return egg.BohioError.ClutchHomeNotFound;
+        };
     }
-
-    const home = try homeDir();
-    return fs.path.joinWindows(allocator, &[_][]const u8{ home, ".clutch" }) catch |_| {
-        return egg.BohioError.ClutchHomeNotFound;
-    };
 }
